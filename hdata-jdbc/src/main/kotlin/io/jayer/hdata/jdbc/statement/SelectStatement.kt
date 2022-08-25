47d4c1703758c0c3e55c4be0e901163689099402
xuhai <xuhai@u51.com>
2022-08-25 17:08:34 +0800

    update

--- hdata-jdbc/src/main/kotlin/io/jayer/hdata/jdbc/statement/SelectStatement.kt
+++ hdata-jdbc/src/main/kotlin/io/jayer/hdata/jdbc/statement/SelectStatement.kt
@@ -1,23 +1,29 @@
 package io.jayer.hdata.jdbc.statement
 
-import java.io.Serializable
-
 /**
  * @author wuya
  * @date 2022-08-25
  */
-data class SelectStatement(
+class SelectStatement(
     val columns: List<String> = emptyList(),
     val table: String = "",
     val where: List<String> = emptyList(),
-) : Serializable {
+) : Statement {
+
+    fun copy(
+        columns: List<String> = this.columns,
+        table: String = this.table,
+        where: List<String> = this.where
+    ): SelectStatement {
+        return SelectStatement(columns, table, where)
+    }
 
     fun columns(vararg columns: String): SelectStatement {
         return columns(columns.toList())
     }
 
     fun columns(columns: List<String>): SelectStatement {
-        return this.copy(columns = columns)
+        return copy(columns = columns)
     }
 
     fun appendWhere(vararg condition: String): SelectStatement {
@@ -25,10 +31,10 @@ data class SelectStatement(
     }
 
     fun appendWhere(conditions: List<String>): SelectStatement {
-        return this.copy(where = this.where.toMutableList().apply { addAll(conditions) })
+        return copy(where = where.toMutableList().apply { addAll(conditions) })
     }
 
-    fun buildSql(): String {
+    override fun buildSql(): String {
         var sql = "SELECT ${columns.filter { it.isNotBlank() }.joinToString(", ")} FROM $table"
         val conditions = where.filter { it.isNotBlank() }
         if (conditions.isNotEmpty()) {
